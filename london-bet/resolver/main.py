d4a59c14b17e18e68baa71e9880e52338324ba07
teo <tmavroeidakos@gmail.com>
2024-10-23 16:13:27 +0300

    feat: add low memory flag.

--- london-bet/resolver/main.py
+++ london-bet/resolver/main.py
@@ -4,14 +4,15 @@ import weather.algo as weather
 
 parser = argparse.ArgumentParser(description='Decide if an event occurred based on weather data provided as input')
 parser.add_argument('--file', '-f', required=True, help='the file with the weather data')
+parser.add_argument('--lowmem', '-lowmem', required=True, type=bool, help='use limited memory resources or not')
 parser.add_argument('--latitude', '-lat', required=False, type=float, help='the latitude of the point of interest (search within 5km radius)')
 parser.add_argument('--longitude', '-lon', required=False, type=float, help='the longitude of the point of interest (search within 5km radius)')
 
 
 args = parser.parse_args()
-path, lat, lon = args.file, args.latitude, args.longitude
+path, lowmem, lat, lon = args.file, args.lowmem args.latitude, args.longitude
 algo = weather.has_verified_metrics
-decision = decide(path, algo, True, lat, lon)
+decision = decide(path, algo, lowmem, lat, lon)
 print(decision)
 
 
