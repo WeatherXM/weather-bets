a32dd36240d8209391c229e8fde78ea11570ba9b
teo <tmavroeidakos@gmail.com>
2024-11-20 10:51:18 +0200

    chore: handle zero eligible devices case.

--- london-bet/resolver/main.py
+++ london-bet/resolver/main.py
@@ -11,6 +11,9 @@ parser.add_argument('--longitude', '-lon', required=False, type=float, help='the
 args = parser.parse_args()
 path, lowmem, lat, lon = args.file, args.lowmem, args.latitude, args.longitude
 decision = decide(path, lowmem)
-print('AVG TEMP: {} Celsius'.format(decision))
+if type(decision) == str:
+    print(decision)
+else:
+    print('AVG TEMP: {} Celsius'.format(decision))
 
 
